527967d0663bfe0c3094ca9f54d15aabe70ac51a
Giancarlos Salas <giansalex@gmail.com>
2021-10-14 14:19:30 -0500

    Add store code db schema

---
+++ database/codes.go
@@ -0,0 +1,14 @@
+package database
+
+import (
+	"github.com/disperze/wasmx/types"
+)
+
+// SaveCode allows to save the given code into the database.
+func (db Db) SaveCode(code types.Code) error {
+	stmt := `
+INSERT INTO codes (code_id, source, builder, creator, creation_time, height) 
+VALUES ($1, $2, $3, $4, $5, $6)`
+	_, err := db.Sql.Exec(stmt, code.CodeID, code.Source, code.Builder, code.Creator, code.CreatedTime, code.Height)
+	return err
+}
