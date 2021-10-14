1793807ea52829b95d182dfa949eee965e36f582
Giancarlos Salas <giansalex@gmail.com>
2021-10-13 22:48:18 -0500

    Add database tests

---
+++ database/database_test.go
@@ -0,0 +1,109 @@
+package database_test
+
+import (
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+	"regexp"
+	"strings"
+	"testing"
+
+	junodb "github.com/desmos-labs/juno/db"
+	"github.com/desmos-labs/juno/types/logging"
+
+	juno "github.com/desmos-labs/juno/types"
+
+	wasmtypes "github.com/CosmWasm/wasmd/x/wasm/types"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/stretchr/testify/suite"
+
+	"github.com/disperze/wasmx/database"
+	"github.com/disperze/wasmx/types/config"
+
+	_ "github.com/proullon/ramsql/driver"
+)
+
+type DbTestSuite struct {
+	suite.Suite
+
+	database *database.Db
+	testData TestData
+}
+
+type TestData struct {
+	contract wasmtypes.ContractInfo
+}
+
+func (suite *DbTestSuite) SetupTest() {
+	// Setup test data
+	suite.setupTestData()
+
+	// Build the database
+	encodingConfig := config.MakeEncodingConfig()
+	databaseConfig := juno.NewDatabaseConfig(
+		"wasmx",
+		"localhost",
+		5433,
+		"wasmx",
+		"password",
+		"",
+		"public",
+		10,
+		10,
+	)
+
+	db, err := database.Builder(junodb.NewContext(databaseConfig, &encodingConfig, logging.DefaultLogger()))
+	suite.Require().NoError(err)
+
+	desmosDb, ok := (db).(*database.Db)
+	suite.Require().True(ok)
+
+	// Delete the public schema
+	_, err = desmosDb.Sql.Exec(fmt.Sprintf(`DROP SCHEMA %s CASCADE;`, databaseConfig.GetSchema()))
+	suite.Require().NoError(err)
+
+	// Re-create the schema
+	_, err = desmosDb.Sql.Exec(fmt.Sprintf(`CREATE SCHEMA %s;`, databaseConfig.GetSchema()))
+	suite.Require().NoError(err)
+
+	dirPath := "schema"
+	dir, err := ioutil.ReadDir(dirPath)
+	for _, fileInfo := range dir {
+		if !strings.HasSuffix(fileInfo.Name(), ".sql") {
+			continue
+		}
+
+		file, err := ioutil.ReadFile(filepath.Join(dirPath, fileInfo.Name()))
+		suite.Require().NoError(err)
+
+		commentsRegExp := regexp.MustCompile(`/\*.*\*/`)
+		requests := strings.Split(string(file), ";")
+		for _, request := range requests {
+			_, err := desmosDb.Sql.Exec(commentsRegExp.ReplaceAllString(request, ""))
+			suite.Require().NoError(err)
+		}
+	}
+
+	suite.database = desmosDb
+}
+
+func (suite *DbTestSuite) setupTestData() {
+	sender, _ := sdk.AccAddressFromBech32("cosmos1qpzgtwec63yhxz9hesj8ve0j3ytzhhqaqxrc5d")
+	suite.testData = TestData{
+		contract: wasmtypes.NewContractInfo(
+			2,
+			sender,
+			sender,
+			"First Contract",
+			&wasmtypes.AbsoluteTxPosition{
+				BlockHeight: 1,
+				TxIndex:     1,
+			},
+		),
+	}
+}
+
+func TestDatabaseTestSuite(t *testing.T) {
+	suite.Run(t, new(DbTestSuite))
+}
--- go.mod
+++ go.mod
@@ -7,6 +7,8 @@ require (
 	github.com/cosmos/cosmos-sdk v0.42.9
 	github.com/desmos-labs/juno v0.0.0-20210824062438-d6fbb0879808
 	github.com/jmoiron/sqlx v1.3.4
+	github.com/proullon/ramsql v0.0.0-20210730175921-2692f3496a21
+	github.com/stretchr/testify v1.7.0
 )
 
 replace github.com/gogo/protobuf => github.com/regen-network/protobuf v1.3.3-alpha.regen.1
