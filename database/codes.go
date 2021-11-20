d16c52cb99de08f7555a14001e8019565549b0fa
Giancarlos Salas <giansalex@gmail.com>
2021-11-20 17:18:40 -0500

    Fix remove fields (source, builder)

--- database/codes.go
+++ database/codes.go
@@ -7,7 +7,7 @@ import (
 // SaveCode allows to save the given code into the database.
 func (db Db) SaveCode(code types.Code) error {
 	stmt := `
-INSERT INTO codes (code_id, source, builder, creator, creation_time, height) 
+INSERT INTO codes (code_id, creator, creation_time, height) 
 VALUES ($1, $2, $3, $4, $5, $6)`
 	_, err := db.Sql.Exec(stmt, code.CodeID, code.Creator, code.CreatedTime, code.Height)
 	return err
