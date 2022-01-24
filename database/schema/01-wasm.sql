f8682d6552caf123ffc39ac099a570f99a6d8fc2
Giancarlos Salas <giansalex@gmail.com>
2022-01-24 17:23:17 -0500

    Update sql schema

--- database/schema/01-wasm.sql
+++ database/schema/01-wasm.sql
@@ -1,3 +1,14 @@
+CREATE TABLE codes
+(
+    code_id       BIGINT                      NOT NULL UNIQUE PRIMARY KEY,
+    creator       TEXT                        NOT NULL DEFAULT '',
+    creation_time TEXT                        NOT NULL DEFAULT '',
+    version       TEXT                        NULL,
+    height        BIGINT                      NOT NULL
+);
+
+CREATE INDEX codes_creator_index ON codes (creator);
+
 CREATE TABLE contracts
 (
     address       TEXT                        NOT NULL UNIQUE PRIMARY KEY,
@@ -15,3 +26,12 @@ CREATE TABLE contracts
 
 CREATE INDEX contracts_code_id_index ON contracts (code_id);
 CREATE INDEX contracts_creator_index ON contracts (creator);
+
+CREATE TABLE tokens
+(
+    address      TEXT                        NOT NULL UNIQUE PRIMARY KEY,
+    name         TEXT                        NOT NULL DEFAULT '',
+    symbol       TEXT                        NOT NULL DEFAULT '',
+    decimals     TINYINT                     NOT NULL DEFAULT 0,
+    supply       BIGINT                      NOT NULL DEFAULT 0
+);
