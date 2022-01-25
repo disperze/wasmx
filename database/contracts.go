238e7c6862e5b02c4fd148787eda5e994113a71a
Giancarlos Salas <giansalex@gmail.com>
2022-01-25 11:16:01 -0500

    Handle wasm msg (migrate, update admin)

--- database/contracts.go
+++ database/contracts.go
@@ -20,3 +20,19 @@ UPDATE contracts SET tx=tx+$2, gas=gas+$3, fees=fees+$4 WHERE address = $1`
 	_, err := db.Sql.Exec(stmt, contract, tx, gas, fees)
 	return err
 }
+
+// SaveContractCodeID save new contract CodeID.
+func (db Db) SaveContractCodeID(contract string, codeID uint64) error {
+	stmt := `
+UPDATE contracts SET code_id = $2 WHERE address = $1`
+	_, err := db.Sql.Exec(stmt, contract, codeID)
+	return err
+}
+
+// UpdateContractAdmin update contract admin.
+func (db Db) UpdateContractAdmin(contract string, admin string) error {
+	stmt := `
+UPDATE contracts SET admin = $2 WHERE address = $1`
+	_, err := db.Sql.Exec(stmt, contract, admin)
+	return err
+}
