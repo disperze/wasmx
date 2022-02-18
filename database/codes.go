4eac4decbafd41b9ce868a23c5b754fb1db23857
Giancarlos Salas <giansalex@gmail.com>
2022-02-18 16:38:07 -0600

    Fix sql params

--- database/codes.go
+++ database/codes.go
@@ -8,7 +8,7 @@ import (
 func (db Db) SaveCode(code types.Code) error {
 	stmt := `
 INSERT INTO codes (code_id, creator, creation_time, height) 
-VALUES ($1, $2, $3, $4, $5, $6)`
+VALUES ($1, $2, $3, $4)`
 	_, err := db.Sql.Exec(stmt, code.CodeID, code.Creator, code.CreatedTime, code.Height)
 	return err
 }
